edcf74524e9712288f8235a1bc75640c15cdfaa5
LeonardoCavi <oliveira.leoc@hotmail.com>
2023-10-07 21:01:24 -0300

    Feature - Add e Configurando o Serilog no projeto e add os logs nas Controllers.

---
+++ ONGAnimaisAPI/ONGAnimaisAPI.API/Configurations/SerilogConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+namespace ONGAnimaisAPI.API.Configurations
+{
+    public static class SerilogConfiguration
+    {
+        public static void AddSerilogConfiguration(this IServiceCollection services,
+                                                   IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(configuration)
+                .CreateLogger();
+        }
+
+        public static void UseSerilogConfiguration(this ConfigureHostBuilder configureHostBuilder)
+        {
+            configureHostBuilder.UseSerilog();
+        }
+    }
+}
